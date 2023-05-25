3ba0cd4d79025af637cbfef1ed1b6de8219cc62b
indrayanikalake <indraayanikalaake333@gmail.com>
2023-05-25 18:24:41 +0530

    one pass

--- abc/src/App.js
+++ abc/src/App.js
@@ -2,28 +2,14 @@
 import Expense from './components/Expense';
 
 function App(){
- // const locationOFExpenditure='commercial';
- const expenses = [
-  {
-    expenseDate: new Date(2023, 2, 28),
-    expenseTitle: 'Car Insurance',
-    expenseAmount: 249,
-    locationOfExpenditure: 'Auto Insurance Company'
-  },
-  {
-    expenseDate: new Date(2023, 3, 15),
-    expenseTitle: 'Groceries',
-    expenseAmount: 50,
-    locationOfExpenditure: 'Local Supermarket'
-  },
-  // Add more expenses here
-];
+  const locationOFExpenditure='commercial';
+ 
 
   return(
 
     <div>
       
-        <Expense expenses={expenses}/>
+        <Expense locationOFExpenditure={locationOFExpenditure}/>
     
     </div>
 
--- abc/src/components/Expense.js
+++ abc/src/components/Expense.js
@@ -3,20 +3,22 @@ import './Expense.css';
 import React from 'react';
 
 export default function Expense(props) {
-    const { expenses } = props;
+    const expenseDate=new Date(2023,2,28);
+    const expenseTitle='Car Insurance';
+    const expenseAmount=249;
   return (
 
     <div>
-    {expenses && expenses.map((expense, index) => (
-      <div className='expense-item' key={index}>
-        <div>{expense.expenseDate.toISOString()}</div>
+
+      <div className='expense-item' >
+        <div>{expenseDate.toISOString()}</div>
         <div className='expense-item__description'>
-          <h3>{expense.expenseTitle}</h3>
-          <div className='expense-item__price'>${expense.expenseAmount}</div>
-          <div>{expense.locationOfExpenditure}</div>
+          <h3>{expenseTitle}</h3>
+          <div className='expense-item__price'>${expenseAmount}</div>
+          <div>{props.locationOfExpenditure}</div>
         </div>
       </div>
-    ))}
+   
   </div>
   )
 }
