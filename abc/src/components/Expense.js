c10504075beddcc29d0088eba4ecaaa2a21d7f33
indrayanikalake <indraayanikalaake333@gmail.com>
2023-05-25 16:01:55 +0530

    ExpenseItem

---
+++ abc/src/components/Expense.js
@@ -0,0 +1,17 @@
+
+
+import React from 'react'
+
+export default function Expense() {
+  return (
+    <div>
+      <h4>Expense Items</h4>
+        <ul>
+          <li>Food Rs 10</li>
+          <li>Petrol Rs 100</li>
+          <li>Movies Rs 200</li>
+        </ul>
+    </div>
+  )
+}
+
