6b6793282f7bc8c3ac46b65dde2dd2d9102e071f
indrayanikalake <indraayanikalaake333@gmail.com>
2023-05-25 17:00:06 +0530

    styling

--- abc/src/components/Expense.js
+++ abc/src/components/Expense.js
@@ -1,16 +1,20 @@
 
-
-import React from 'react'
+import './Expense.css';
+import React from 'react';
 
 export default function Expense() {
+    const expenseDate=new Date(2023,2,28);
+    const expenseTitle='Car Insurance';
+    const expenseAmount=249;
+    const locationOfExpenditure = 'Auto Insurance Company';
   return (
-    <div>
-      <h4>Expense Items</h4>
-        <ul>
-          <li>Food Rs 10</li>
-          <li>Petrol Rs 100</li>
-          <li>Movies Rs 200</li>
-        </ul>
+    <div className='expense-item'>
+     <div>{expenseDate.toISOString()}</div>
+     <div className='expense-item__description'>
+        <h2>{expenseTitle}</h2>
+        <div className='expense-item__price'>${expenseAmount}</div>
+        <div>{locationOfExpenditure}</div>
+     </div>
     </div>
   )
 }
