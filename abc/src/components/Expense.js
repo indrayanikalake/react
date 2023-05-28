7bf0a745d572002cd559d4e176de61c334be6eb5
indrayanikalake <indraayanikalaake333@gmail.com>
2023-05-28 11:27:23 +0530

    delete operatio

--- abc/src/components/Expense.js
+++ abc/src/components/Expense.js
@@ -5,16 +5,25 @@ import Package from './UI/Package';
 
 
 const Expense = (props) => {
+  const deleteExpenseHandler = () => {
+    console.log('deleted');
+    const expenseElement = document.getElementById(props.id);
+    if (expenseElement) {
+      expenseElement.remove();
+    }
+  };
+  
   return (
-    
-        <div className='main'>
+    <Package className='card'>
+   
     <div className='expense-details'>
       <ExpenseDetails date={props.date} location={props.locationOfExpenditure} />
       <div>{props.title}</div>
       <div className='amount'>${props.amount}</div>
-     
+      <button onClick={deleteExpenseHandler}>Delete Expense</button>
     </div>
-    </div>
+   
+    </Package>
     
   )
 }
