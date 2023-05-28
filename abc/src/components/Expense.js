d1ff43c562ace1781cdbc65e75184618247f2020
indrayanikalake <indraayanikalaake333@gmail.com>
2023-05-28 14:57:27 +0530

    change Amount

--- abc/src/components/Expense.js
+++ abc/src/components/Expense.js
@@ -6,11 +6,27 @@ import Package from './UI/Package';
 
 
 const Expense = (props) => {
+  //let title=props.title;
+  const [title, setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount);
+
+  const changeAmountHandler = () => {
+    setAmount(100);
+  };
+
+
  const deleteExpenseHandler=(e)=>{
+ 
+  console.log('deleted');
     e.target.parentNode.remove();
   
   }
-
+  const changeEpenseHandler=()=>{
+    //title='updated';
+    setTitle('updated');
+    //console.log(title);
+  }
+  
   //react code
  /* const [isDeleted, setIsDeleted] = useState(false);
 
@@ -27,9 +43,11 @@ const Expense = (props) => {
    
     <div className='expense-details'>
       <ExpenseDetails date={props.date} location={props.locationOfExpenditure} />
-      <div>{props.title}</div>
+      <div>{title}</div>
       <div className='amount'>${props.amount}</div>
       <button onClick={deleteExpenseHandler}>Delete Expense</button>
+      <button onClick={changeEpenseHandler}>Change Title</button>
+      <button onClick={changeAmountHandler}>Change Amount</button>
     </div>
    
     </Package>
