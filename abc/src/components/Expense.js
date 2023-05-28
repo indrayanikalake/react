d83361964991eef31a96a06fc484700c728d14b6
indrayanikalake <indraayanikalaake333@gmail.com>
2023-05-28 11:56:14 +0530

    delete event listner

--- abc/src/components/Expense.js
+++ abc/src/components/Expense.js
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import './Expense.css';
 import ExpenseDetails from './ExpenseDetails';
 import Package from './UI/Package';
 
 
 const Expense = (props) => {
-  const deleteExpenseHandler = () => {
-    console.log('deleted');
-    const expenseElement = document.getElementById(props.id);
-    if (expenseElement) {
-      expenseElement.remove();
-    }
-  };
+ const deleteExpenseHandler=(e)=>{
+    e.target.parentNode.remove();
   
+  }
+
+  //react code
+ /* const [isDeleted, setIsDeleted] = useState(false);
+
+  const deleteExpenseHandler = () => {
+    setIsDeleted(true);
+  };
+
+  if (isDeleted) {
+    return null; // Render null if the expense is deleted
+  } */
+
   return (
     <Package className='card'>
    
