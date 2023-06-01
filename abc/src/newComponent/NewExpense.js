06ad07272370fd6cbcb77399fedc485c287bfa48
indrayanikalake <indraayanikalaake333@gmail.com>
2023-06-01 19:00:02 +0530

    challanges on conditional rendering

--- abc/src/newComponent/NewExpense.js
+++ abc/src/newComponent/NewExpense.js
@@ -1,21 +1,57 @@
-import React from "react";
-import DesignPatternWithState from "./DesignPatternWithState";
+import ExpenseForm from "./ExpenseForm";
+import { useState } from "react";
+const NewExpense = () => {
+    const [expenses, setExpenses] = useState([]);
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [enteredDate, setEnteredDate] = useState('');
+    const [showForm, setShowForm] = useState(false);
 
-const NewExpense = (props) => {
-    const saveExpenseDataHandler=(enteredExpenseData)=>{
-        const submittedData={
-            ...enteredExpenseData,
-            id:Math.random().toString(),
-           
-        }
-        console.log(submittedData);
-        props.onAddExpense(submittedData);
-    }
-  return(
-    <div>
-        <DesignPatternWithState onSaveExpenseData={saveExpenseDataHandler} />
-    </div>
-  )
+    const addExpenseHandler = (expenseData) => {
+      setExpenses((prevExpenses) => {
+        return [...prevExpenses, expenseData];
+      });
+      setShowForm(false);
+    };
+
+    const showFormHandler = () => {
+        setShowForm(true);
+      };
+    
+      const hideFormHandler = () => {
+        setShowForm(false);
+      };
+
+    const titleChangeHandler = (event) => {
+      setEnteredTitle(event.target.value);
+    };
+
+    const amountChangeHandler = (event) => {
+      setEnteredAmount(event.target.value);
+    };
+
+    const dateChangeHandler = (event) => {
+      setEnteredDate(event.target.value);
+    };
+
+    return (
+      <div>
+        <h1>Expense Tracker</h1>
+        {!showForm && <button onClick={showFormHandler}>Add Expense</button>}
+        {showForm && (
+        <ExpenseForm
+          onAddExpense={addExpenseHandler}
+          enteredTitle={enteredTitle}
+          enteredAmount={enteredAmount}
+          enteredDate={enteredDate}
+          onTitleChange={titleChangeHandler}
+          onAmountChange={amountChangeHandler}
+          onDateChange={dateChangeHandler}
+        />
+        )}
+        {/* Render the expenses here */}
+      </div>
+    );
 }
 
 export default NewExpense;
